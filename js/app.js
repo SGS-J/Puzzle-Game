25574d378ae8dd0eee8b9de765d84c00688b287a
SGS-J <santigarsan07@gmail.com>
2020-08-26 08:50:49 -0500

    Styles of timer addede

--- js/app.js
+++ js/app.js
@@ -1,13 +1,17 @@
-var app = {
+const app = {
   button: document.querySelector("footer button"),
   imgPanel: document.querySelector(".panel-img"),
   figures: document.querySelectorAll(".panel-img img, .panel-img div"),
+  timer: document.querySelector("footer .timer"),
+  score: document.querySelector("footer .best-time"),
   gameLogic: new GameLogic(),
   gameEvent: new GameEvent(),
-  gameState: new GameState(GameState.states.ready)
+  gameState: new GameState(GameState.states.ready),
+  showWinDialog: () => {
+
+  }
 };
 
-app.button.addEventListener("click", app.gameEvent.initGame);
 app.figures.forEach((fig) => {
   fig.addEventListener("dragstart", e => {
     e.preventDefault();
--- js/state.js
+++ js/state.js
@@ -17,23 +17,28 @@ class GameState {
   constructor(state) {
     this.actualState = state;
     this.timer = null;
+    this.timeScore = 0;
   }
 
   static states = {
     ready: new State("ready", () => {
       app.button.textContent = "Play!";
+      app.button.removeEventListener("click", app.gameEvent.undo);
+      app.button.addEventListener("click", app.gameEvent.initGame);
     }),
 
     playing: new State("playing", () => {
       let count = 0;
       this.timer = setInterval(() => {
-        document.querySelector(".result").textContent = count;
         ++count;
+        this.timeScore = count;
+        app.timer.textContent = `Time: ${count}`;
       }, 1000);
-      // Changes button actions
+
       app.button.textContent = "Undo";
       app.button.removeEventListener("click", app.gameEvent.initGame);
       app.button.addEventListener("click", app.gameEvent.undo);
+
       // Add listeners to img in board
       Array.from(app.figures).forEach((fig) => {
         if (fig.localName !== "div")
@@ -45,13 +50,23 @@ class GameState {
 
     win: new State("win", () => {
       clearInterval(this.timer);
+      // Removes listeners from img in board
       Array.from(app.figures).forEach((fig) => {
         if (fig.localName !== "div")
           fig.removeEventListener("click", (e) =>
             app.gameEvent.moveGrid(e.target)
           );
       });
-      document.querySelector(".result").textContent = "You win!";
+      if (localStorage.getItem("score")) {
+        let scoreSaved = localStorage.getItem("score");
+        localStorage.setItem(
+          "score",
+          `${scoreSaved < this.timeScore ? this.timeScore : scoreSaved}`
+        );
+      } else {
+        localStorage.setItem("score", this.timeScore);
+      }
+      app.showWinDialog();
     }),
   };
 
